ad301125f7b32eb8b8dceca79278b48982f74770
atkinsta <atkinstaa@gmail.com>
2018-07-18 12:50:22 -0400

    User authentication is in

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,10 @@
 var db = require("../models");
+var passport = require("../config/passport.js");
 
 module.exports = function(app) {
 
     app.get("/api/snippets", (req, res) => {
-        //get all snippets
+        
     });
 
     app.get("/api/snippets/:author", (req, res) => {
@@ -29,4 +30,51 @@ module.exports = function(app) {
     app.delete("/api/snippets/:id", (req, res) => {
         //delete snippet where id = ?
     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.post("/api/login", passport.authenticate("local"), (req, res) => {
+        res.json("/home");
+    });
+
+    app.post("/api/signup", (req, res) => {
+        db.User.create({
+            username: req.body.username,
+            fullName: req.body.fullName,
+            password: req.body.password
+        }).then( () => {
+            res.redirect(307, "/api/login");
+        }).catch(err => {
+            console.log(err);
+            res.json(err);
+        });
+    });
+
+    app.get("/logout", (req, res) => {
+        req.logout();
+        res.redirect("/");
+    });
+
+    app.get("/api/user_data", (req, res) => {
+        if (!req.user) {
+            res.json();
+        }
+        else {
+            res.json({
+                email: req.user.email,
+                id: req.user.id
+            });
+        }
+    });
+
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var passport = require("./config/passport.js");
 
 var db = require("./models");
 
@@ -12,6 +13,9 @@ var PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use(session({ secret: "snippy man", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
