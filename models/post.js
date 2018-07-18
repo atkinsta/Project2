288bbf9133104ea435b87c7bcd63f7818085a381
Victoria Paul <victoriapaul@CodeBook-Pro.local>
2018-07-18 11:10:21 -0400

    models added

---
+++ models/post.js
@@ -0,0 +1,43 @@
+// Create Posts Table
+// title, language, code, description...
+
+module.exports = function (sequelize, DataTypes) {
+    var Post = sequelize.define("Post", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: true,
+                max: 50
+            }
+        },
+        language: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: true,
+                max: 15
+            }
+        },
+        codeBlock: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notNull: true,
+                max: 500
+            }
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: true
+            }
+        }
+    });
+
+
+    // add associations
+
+    return Post;
+};
