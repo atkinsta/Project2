633aac86082ee63f9b0ec23da2db983fbdd747a9
atkinsta <atkinstaa@gmail.com>
2018-07-18 12:56:34 -0400

    broken login into seperate file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,4 @@
 var db = require("../models");
-var passport = require("../config/passport.js");
 
 module.exports = function(app) {
 
@@ -30,51 +29,4 @@ module.exports = function(app) {
     app.delete("/api/snippets/:id", (req, res) => {
         //delete snippet where id = ?
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-    app.post("/api/login", passport.authenticate("local"), (req, res) => {
-        res.json("/home");
-    });
-
-    app.post("/api/signup", (req, res) => {
-        db.User.create({
-            username: req.body.username,
-            fullName: req.body.fullName,
-            password: req.body.password
-        }).then( () => {
-            res.redirect(307, "/api/login");
-        }).catch(err => {
-            console.log(err);
-            res.json(err);
-        });
-    });
-
-    app.get("/logout", (req, res) => {
-        req.logout();
-        res.redirect("/");
-    });
-
-    app.get("/api/user_data", (req, res) => {
-        if (!req.user) {
-            res.json();
-        }
-        else {
-            res.json({
-                email: req.user.email,
-                id: req.user.id
-            });
-        }
-    });
-
 };
