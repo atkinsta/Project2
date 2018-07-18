d7b438f73e14357158382178f63f24daec2b96db
atkinsta <atkinstaa@gmail.com>
2018-07-18 12:58:45 -0400

    Added login routes to server

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.set("view engine", "handlebars");
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+require("./routes/loginRoutes")(app);
 
 var syncOptions = { force: false };
 
